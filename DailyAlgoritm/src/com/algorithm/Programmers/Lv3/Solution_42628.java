308fa00f238c6b0de5cf90161b8bc5f9e4bf1049
MaengJoonYoung <jymaeng95@gmail.com>
2020-11-14 15:34:12 +0900

    /* 20.11.14 Daily Algorithm */

--- DailyAlgoritm/src/com/algorithm/Programmers/Lv3/Solution_42628.java
+++ DailyAlgoritm/src/com/algorithm/Programmers/Lv3/Solution_42628.java
@@ -12,8 +12,10 @@ public class Solution_42628 {
 
     public static int[] solution(String[] operations) {
         int[] answer = new int[2];
+        //최소 힙, 최대 힙
         PriorityQueue<Integer> pq = new PriorityQueue<>();
         PriorityQueue<Integer> maxPq = new PriorityQueue<>(Collections.reverseOrder());
+
         for (String op : operations) {
             StringTokenizer st = new StringTokenizer(op);
             String judge = st.nextToken();
@@ -23,16 +25,22 @@ public class Solution_42628 {
             if (pq.size() < 1 && judge.equals("D"))
                 continue;
 
+            //삽입 시 최소 힙, 최대 힙에 value 넣기
             if (judge.equals("I")) {
                 pq.offer(value);
                 maxPq.offer(value);
                 continue;
             }
-            if(value == -1) {
+
+            //나머지 경우는 D이면서 value값이 1인지 -1인지 이므로
+            //0보다 작은 경우 최소 힙에서 poll후 최대힙에서 해당 원소 삭제
+            if(value < 0) {
                 int min = pq.poll();
                 maxPq.remove(min);
                 continue;
             }
+
+            //최대 힙에서 poll후 최소힙에서 해당 원소 삭제
             int max = maxPq.poll();
             pq.remove(max);
         }
