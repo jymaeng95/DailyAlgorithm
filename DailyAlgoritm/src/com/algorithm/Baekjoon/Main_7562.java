054b4e22142464795919b4b407f49b72ac63a929
MaengJoonYoung <jymaeng95@gmail.com>
2020-12-03 21:37:19 +0900

    /* 20.12.03 Daily Algorithm */

--- DailyAlgoritm/src/com/algorithm/Baekjoon/Main_7562.java
+++ DailyAlgoritm/src/com/algorithm/Baekjoon/Main_7562.java
@@ -27,55 +27,66 @@ public class Main_7562 {
             return y;
         }
     }
-    public static int[] dx = {-2,-2,-1,-1,1,1,2,2,};
-    public static int[] dy = {-1,1,-2,2,-2,2,-1,1};
+
+    public static int[] dx = {-2, -2, -1, -1, 1, 1, 2, 2,};
+    public static int[] dy = {-1, 1, -2, 2, -2, 2, -1, 1};
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int TC = Integer.parseInt(br.readLine());
-        for(int i=0;i<TC;i++){
+        for (int i = 0; i < TC; i++) {
             int N = Integer.parseInt(br.readLine());
-            int[][] board = new int[N][N];
             boolean[][] visited = new boolean[N][N];
 
             StringTokenizer st = new StringTokenizer(br.readLine());
 
             int startX = Integer.parseInt(st.nextToken());
             int startY = Integer.parseInt(st.nextToken());
-            Knight startKnight = new Knight(startX,startY);
+            Knight startKnight = new Knight(startX, startY);
 
             st = new StringTokenizer(br.readLine());
             int endX = Integer.parseInt(st.nextToken());
             int endY = Integer.parseInt(st.nextToken());
-            Knight endKnight = new Knight(endX,endY);
+            Knight endKnight = new Knight(endX, endY);
 
-            bfs(startKnight,endKnight,N,visited);
-
+            if (startKnight.getX() == endKnight.getX() && startKnight.getY() == endKnight.getY()) {
+                bw.write(String.valueOf(0));
+                bw.newLine();
+                continue;
+            }
+            int count = bfs(startKnight, endKnight, N, visited);
+            bw.write(String.valueOf(count));
+            bw.newLine();
         }
 
         br.close();
         bw.close();
     }
 
-    private static void bfs(Knight startKnight, Knight endKnight, int N, boolean[][] visited) {
+    private static int bfs(Knight startKnight, Knight endKnight, int N, boolean[][] visited) {
         Queue<Knight> que = new LinkedList<>();
         que.offer(startKnight);
         int count = 0;
-        while(!que.isEmpty()){
-            for(int i=0;i<que.size();i++){
+        while (!que.isEmpty()) {
+            int size = que.size();
+            for (int i = 0; i < size; i++) {
                 Knight knight = que.poll();
-                if(visited[knight.getX()][knight.getY()])
+                if (visited[knight.getX()][knight.getY()])
                     continue;
                 visited[knight.getX()][knight.getY()] = true;
-                for(int j=0;j< dx.length;j++){
+                for (int j = 0; j < dx.length; j++) {
                     int nx = knight.getX() + dx[j];
                     int ny = knight.getY() + dy[j];
-                    if(nx >= 0 && nx < N && ny >= 0 && ny < N)
-                        que.offer(new Knight(nx,ny));
+                    if (nx == endKnight.getX() && ny == endKnight.getY())
+                        return count + 1;
+                    if (nx >= 0 && nx < N && ny >= 0 && ny < N)
+                        que.offer(new Knight(nx, ny));
                 }
             }
             count++;
         }
+        return count;
     }
 }
