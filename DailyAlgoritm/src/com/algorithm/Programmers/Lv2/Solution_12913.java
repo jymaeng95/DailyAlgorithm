4ecc0968fef2f6708b68a148b50660f22ca06d42
MaengJoonYoung <jymaeng95@gmail.com>
2020-12-08 13:57:06 +0900

    /* 20.12.08 Daily Algorithm */

--- DailyAlgoritm/src/com/algorithm/Programmers/Lv2/Solution_12913.java
+++ DailyAlgoritm/src/com/algorithm/Programmers/Lv2/Solution_12913.java
@@ -7,13 +7,17 @@ public class Solution_12913 {
         System.out.println(solution(land));
     }
     static int solution(int[][] land) {
+
         int[][] dp = new int[land.length][4];
         int idx = dp.length-1;
+
         for(int i=0;i<4;i++)
             dp[0][i] = land[0][i];
+
         for(int i=1;i<land.length;i++){
             for(int j=0;j<4;j++){
                 int max = 0;
+                //점화식 max(memo한 부분과 현재 값을 더해 최대값)
                 for(int k=0;k<4;k++){
                     if(j!=k)
                         max = Math.max(max, land[i][j] + dp[i-1][k]);
