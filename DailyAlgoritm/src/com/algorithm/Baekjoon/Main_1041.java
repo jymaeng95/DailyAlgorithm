2974bcb99aa0cf5a147c240071b8ea23513647c3
MaengJoonYoung <jymaeng95@gmail.com>
2020-12-10 23:54:49 +0900

    /* 20.12.10 Daily Algorithm */

--- DailyAlgoritm/src/com/algorithm/Baekjoon/Main_1041.java
+++ DailyAlgoritm/src/com/algorithm/Baekjoon/Main_1041.java
@@ -1,12 +1,11 @@
 package com.algorithm.Baekjoon;
 
 import java.io.*;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main_1041 {
-    private static int N;
-    private static int one, two, three;
+    private static long N;
+    private static long one, two, three;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -22,16 +21,17 @@ public class Main_1041 {
         }
 
         //마주보는 면의 인덱스의 합 5이므로 마주보는 면 중 최소값을 고르기
-        int minAF = Math.min(dice[0], dice[5]);
-        int minBE = Math.min(dice[1], dice[4]);
-        int minCD = Math.min(dice[2], dice[3]);
-
+        long minAF = Math.min(dice[0], dice[5]);
+        long minBE = Math.min(dice[1], dice[4]);
+        long minCD = Math.min(dice[2], dice[3]);
+        System.out.println(minAF);
+        System.out.println(minBE);
+        System.out.println(minCD);
         // 1,2,3면 구성
         one = Math.min(minAF, Math.min(minBE, minCD));
         two = Math.min(minAF + minBE, Math.min(minAF + minCD, minBE + minCD));
         three = minAF + minBE + minCD;
 
-        Arrays.sort(dice);
         long sum = minSum(dice);
 
         bw.write(String.valueOf(sum));
@@ -40,11 +40,14 @@ public class Main_1041 {
     }
 
     private static long minSum(int[] dice) {
-        long sum = 0L;
+        long sum = 0;
         if (N == 1) {
-            for (int i = 0; i < 5; i++)
+            long max = Integer.MIN_VALUE;
+            for (int i = 0; i < 6; i++) {
+                max = Math.max(max,dice[i]);
                 sum += dice[i];
-            return sum;
+            }
+            return sum-max;
         }
 
         /*
@@ -52,7 +55,7 @@ public class Main_1041 {
          * 2면 : 4(n-1) + 4*(n-2)
          * 1면 : 4(n-1)(n-2) + (n-2)(n-2)
          */
-        long oneSide = (4 * (N - 1) * (N - 2)) + ((N - 2) * (N - 2));
+        long oneSide = 4 * (N - 1) * (N - 2) + (N - 2) * (N - 2);
         long secondSide = 4 * (N - 1) + 4 * (N - 2);
         long thirdSide = 4;
 
