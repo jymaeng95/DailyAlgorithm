51b95f92a9a9a0ec17b3856ef7c6e628333830c3
zayson <jymaeng95@gmail.com>
2020-12-17 20:39:48 +0900

    /* 20.12.17 Daily Algorithm */

--- DailyAlgoritm/src/com/algorithm/Baekjoon/Main_13335.java
+++ DailyAlgoritm/src/com/algorithm/Baekjoon/Main_13335.java
@@ -37,12 +37,17 @@ public class Main_13335 {
 
         while(!que.isEmpty() || !bridge.isEmpty()){
             count++;
+            //다리의 현재 시간에서 다리에 진입한 시간을 빼줫을 때 다리의 길이보다 크면 다리를 빠져가고 무게를 현재 다리의 무게를 빼준다.
             if(!bridge.isEmpty() && count - bridge.peek().time >= bridgeLength) {
                 curWeight -= bridge.poll().weight;
             }
+
+            //현재 다리의 무게에 다음에 들어올 트럭의 무게를 더햇을 때 다리의 무게보다 작거나 같으면 다리에 진입한다.
             if(!que.isEmpty() && curWeight + que.peek().weight <= bridgeWeight){
                 Truck truck = que.poll();
                 curWeight += truck.weight;
+
+                //다리에 진입한 시간을 다리 큐에 넣어준다.
                 bridge.offer(new Truck(truck.weight,count));
             }
         }
