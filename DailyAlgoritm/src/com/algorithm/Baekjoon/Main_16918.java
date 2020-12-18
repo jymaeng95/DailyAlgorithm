865a4b7869c804cf589b311635326703b13a2449
zayson <jymaeng95@gmail.com>
2020-12-18 22:13:34 +0900

    /* 20.12.18 Daily Algorithm */

--- DailyAlgoritm/src/com/algorithm/Baekjoon/Main_16918.java
+++ DailyAlgoritm/src/com/algorithm/Baekjoon/Main_16918.java
@@ -1,12 +1,24 @@
 package com.algorithm.Baekjoon;
 
 import java.io.*;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main_16918 {
     private static int R, C, N;
     private static final int[] DX = {1, -1, 0, 0};
     private static final int[] DY = {0, 0, -1, 1};
+
+    static class Index {
+        private int x;
+        private int y;
+
+        public Index(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -26,8 +38,7 @@ public class Main_16918 {
             }
         }
 
-        afterBomb(grid);
-
+        afterBomb(grid, 1);
 
         for (String[] arr : grid) {
             for (String s : arr) {
@@ -40,50 +51,54 @@ public class Main_16918 {
         bw.close();
     }
 
-    private static void afterBomb(String[][] grid) {
-        boolean[][] visited = new boolean[R][C];
-        N = (N > 1) ? (N % 4) : N;
-        switch (N) {
-            case 1:
-            case 3:
-                for (int i = 0; i < R; i++) {
-                    for (int j = 0; j < C; j++) {
-                        grid[i][j] = "O";
-                    }
-                }
-                break;
-            case 2:
-                for (int i = 0; i < R; i++) {
-                    for (int j = 0; j < C; j++) {
-                        if (grid[i][j].equals("O") && !visited[i][j]) {
-                            visited[i][j] = true;
-                            affectFourWay(visited, grid, i, j);
-                        }
-                    }
-                }
+    private static void afterBomb(String[][] grid, int time) {
+        Queue<Index> que = new LinkedList<>();
 
-                for (int i = 0; i < R; i++) {
-                    for (int j = 0; j < C; j++) {
-                        if (grid[i][j].equals(".")) {
-                            grid[i][j] = "O";
-                            continue;
-                        }
-                        grid[i][j] = ".";
-                    }
-                }
+        while(time < N) {
+            //큐에 존재하던 폭탄 넣어주기
+            originalBombQue(que,grid);
+
+            // 빈 곳에 폭탄 채우기
+            fillBombInBlank(grid);
+            time += 1;
+            if(time == N)
                 break;
-            default :
-                break;
+
+            //폭탄 터뜨리기
+            while(!que.isEmpty()){
+                Index index = que.poll();
+                grid[index.x][index.y] = ".";
+                affectFourWay(grid,index);
+            }
+            time += 1;
         }
     }
 
-    private static void affectFourWay(boolean[][] visited, String[][] grid, int i, int j) {
-        for (int k = 0; k < DX.length; k++) {
-            int nx = i + DX[k];
-            int ny = j + DY[k];
-            if (nx >= 0 && nx < R && ny >= 0 && ny < C && !visited[nx][ny] && grid[nx][ny].equals(".")) {
-                visited[nx][ny] = true;
-                grid[nx][ny] = "O";
+    private static void affectFourWay(String[][] grid, Index index) {
+        for (int i = 0; i < DX.length; i++) {
+            int nx = index.x + DX[i];
+            int ny = index.y + DY[i];
+            if (nx >= 0 && nx < R && ny >= 0 && ny < C) {
+                grid[nx][ny] = ".";
+            }
+        }
+    }
+
+    private static void originalBombQue(Queue<Index> que, String[][] grid){
+        for (int i = 0; i < R; i++) {
+            for (int j = 0; j < C; j++) {
+                if (grid[i][j].equals("O")) {
+                    que.offer(new Index(i,j));
+                }
+            }
+        }
+    }
+
+    private static void fillBombInBlank(String[][] grid) {
+        for (int i = 0; i < R; i++) {
+            for (int j = 0; j < C; j++) {
+                if(grid[i][j].equals("."))
+                    grid[i][j] = "O";
             }
         }
     }
