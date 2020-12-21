1016ff429eabd666d9eef778a89e5b08784166e7
zayson <jymaeng95@gmail.com>
2020-12-21 11:36:00 +0900

    /* 20.12.21 Daily Algorithm */

---
+++ DailyAlgoritm/src/com/algorithm/Programmers/Lv2/Solution_12953.java
@@ -0,0 +1,28 @@
+package com.algorithm.Programmers.Lv2;
+
+public class Solution_12953 {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3};
+        System.out.println(solution(arr));
+    }
+
+    public static int solution(int[] arr) {
+        int a = arr[0];
+        for(int i=1;i<arr.length;i++){
+            int b = arr[i];
+            int r = 1;
+            int tmp1=a,tmp2=b;
+            if(a<b){
+                tmp1 = b;
+                tmp2 = a;
+            }
+            while(r>0){
+                r = tmp1 % tmp2;
+                tmp1 = tmp2;
+                tmp2 = r;
+            }
+            a = a * b / tmp1;
+        }
+        return a;
+    }
+}
---
+++ DailyAlgoritm/src/com/algorithm/Programmers/Lv2/Solution_12973.java
@@ -0,0 +1,24 @@
+package com.algorithm.Programmers.Lv2;
+
+import java.util.Stack;
+
+public class Solution_12973 {
+    public static void main(String[] args) {
+        String s ="bccbcc";
+        int solution = solution(s);
+        System.out.println(solution);
+    }
+
+    public static int solution(String s) {
+        Stack<Character> stack = new Stack<>();
+        for(int i=0;i<s.length();i++){
+            if(!stack.isEmpty() && stack.peek()==s.charAt(i)) {
+                stack.pop();
+                continue;
+            }
+            stack.push(s.charAt(i));
+        }
+
+        return stack.size() > 0 ? 0 : 1;
+    }
+}
