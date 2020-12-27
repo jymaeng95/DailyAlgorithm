3b3610b57df63f7d18e9c8b6f5ae346d724f1f60
zayson <jymaeng95@gmail.com>
2020-12-27 13:04:25 +0900

    /* 20.12.25 Daily Algorithm */

--- DailyAlgoritm/src/com/algorithm/Programmers/Lv2/Solution_17677.java
+++ DailyAlgoritm/src/com/algorithm/Programmers/Lv2/Solution_17677.java
@@ -7,6 +7,7 @@ public class Solution_17677 {
     public static void main(String[] args) {
         String str1 = "FRANCE";
         String str2 = "french";
+
         System.out.println(solution(str1,str2));
     }
 
@@ -16,6 +17,7 @@ public class Solution_17677 {
         List<String> first  = new ArrayList<>();
         List<String> second = new ArrayList<>();
 
+        //리스트에 알파벳 판별 후 넣기
         for(int i=0;i<splitFirst.length-1;i++){
             if(isAlpha(splitFirst[i]) && isAlpha(splitFirst[i+1]))
                 first.add((splitFirst[i]+splitFirst[i+1]).toUpperCase());
@@ -26,21 +28,26 @@ public class Solution_17677 {
                 second.add((splitSecond[i]+splitSecond[i+1]).toUpperCase());
         }
 
+        //알파벳 쌍이 없는 경우
         if(first.isEmpty() && second.isEmpty())
             return 65536;
 
+        // 다중집합 합집합 개수 = A집합 개수 + B집합 개수 - 교집합 개수
         int setSize = first.size() + second.size();
-        int count = 0;
 
+        //교집합 개수 구하기
+        int count = 0;
         for(String s : first) {
             if(second.contains(s)){
                 second.remove(s);
                 count++;
             }
         }
+
         return (int) Math.floor((double)count / (double)(setSize-count) * 65536);
     }
 
+    //알파벳 판별
     private static boolean isAlpha(String c) {
         String alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
         return alpha.contains(c);
