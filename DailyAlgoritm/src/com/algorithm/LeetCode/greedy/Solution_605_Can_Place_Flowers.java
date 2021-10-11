3a549afd3d8b11b47142e7a15bfa4b392b098176
zayson <jymaeng95@gmail.com>
2021-10-11 22:50:23 +0900

    /* 21.10.11 Daily Algorithm */

--- DailyAlgoritm/src/com/algorithm/LeetCode/greedy/Solution_605_Can_Place_Flowers.java
+++ DailyAlgoritm/src/com/algorithm/LeetCode/greedy/Solution_605_Can_Place_Flowers.java
@@ -2,15 +2,27 @@ package com.algorithm.LeetCode.greedy;
 
 public class Solution_605_Can_Place_Flowers {
     public static void main(String[] args) {
-        int[] flowerbed = {1,0,0,0,1};
-        int n = 1;
+        int[] flowerbed = {0,0,0,1,0,0,1,0,1,0,0,0,1,0,0,0,0,1};
+        int n = 4;
         boolean ans = canPlaceFlowers(flowerbed, n);
         System.out.println(ans);
     }
 
     private static boolean canPlaceFlowers(int[] flowerbed, int n) {
+        int count = 1;
+        int rst = 0;
+        for(int x : flowerbed) {
+            if(x == 0) {
+                count++;
+            }else {
+                rst += (count - 1) / 2;
+                count = 0;
+            }
+        }
 
-
-        return false;
+        if(count > 0) {
+            rst += count / 2;
+        }
+        return rst >= n;
     }
 }
