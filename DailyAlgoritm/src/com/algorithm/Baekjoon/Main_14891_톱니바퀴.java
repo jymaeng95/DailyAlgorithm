7001f36f27b0be34123f486039aba0ad50311b53
zayson <jymaeng95@gmail.com>
2022-01-15 17:11:00 +0900

    /* 22.01.15 Daily Algorithm */

--- DailyAlgoritm/src/com/algorithm/Baekjoon/Main_14891_톱니바퀴.java
+++ DailyAlgoritm/src/com/algorithm/Baekjoon/Main_14891_톱니바퀴.java
@@ -3,7 +3,6 @@ package com.algorithm.Baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main_14891_톱니바퀴 {
@@ -13,9 +12,9 @@ public class Main_14891_톱니바퀴 {
         br = new BufferedReader(new InputStreamReader(System.in));
         int[][] gear = new int[4][8];
         for (int i = 0; i < 4; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine(), "");
+            String[] str = br.readLine().split("");
             for (int j = 0; j < 8; j++) {
-                gear[i][j] = Integer.parseInt(st.nextToken());
+                gear[i][j] = Integer.parseInt(str[j]);
             }
         }
 
@@ -27,74 +26,64 @@ public class Main_14891_톱니바퀴 {
     }
 
     private static int getScore(int[][] gear, int rotate) throws IOException {
-        int leftTwo = 6, leftThree = 6, leftFour = 6;
-        int rightOne = 2, rightTwo = 2, rightThree = 2;
-        int[] left = new int[3];
-        int[] right = new int[3];
-        Arrays.fill(left, 6);
-        Arrays.fill(right, 2);
-        for (int count = 0; count < rotate; count++) {
+        for(int rt = 0; rt < rotate; rt++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int gearNo = Integer.parseInt(st.nextToken());
             int direction = Integer.parseInt(st.nextToken());
-            boolean[] rotation = new boolean[4];
 
-            // 회전 전에 맞 닿은 극 확인
-            checkRotation(left, right,gear, rotation);
-            rotation[gearNo -1] = true;
-
-            checkLeftRotation(gearNo, rotation, gear,left,right,direction);
             //왼쪽 체크
-            for(int i=gearNo-1; i>0; i--) {
-                if(rotation[i-1]) {
-                    checkLeftRotation(i);
-                    direction
-                }
-                else break;
-            }
+            checkLeft(gear, gearNo-1, direction);
+            checkRight(gear, gearNo-1, direction);
+            rotation(gear, gearNo-1, direction);
+        }
 
-            // 오른쪽 체크
-            for(int i=gearNo+1; i<=4; i++) {
-                if(rotation[i-1])
-                    checkRightRotation(i-1);
-                else break;
-            }
+        int score = 0;
+        for(int i=0;i<4;i++) {
+            if(gear[i][0] != 0) score += gear[i][0] * Math.pow(2,i);
+        }
+        return score;
+    }
 
+    private static void checkRight(int[][] gear, int gearNo, int direction) {
+        if(gearNo == 3)
+            return ;
 
-//            checkRotation(left, right, gearNo, rotation);
+        // 돌아가는 바퀴와 오른쪽 바퀴의 극 확인 후 다른 경우 회전
+        if(gear[gearNo][2] != gear[gearNo+1][6]) {
+            checkRight(gear, gearNo+1, -direction);
+            rotation(gear, gearNo+1, -direction);
         }
     }
 
-    private static void checkLeftRotation(int gearNo, boolean[] rotation, int[][] gear, int[] left, int[] right, int direction) {
-        for(int i=gearNo-1; i>0;i--) {
-            if(rotation[i-1]) {
-                if(direction == 1)
-            }
+    private static void checkLeft(int[][] gear, int gearNo, int direction) {
+        // 돌아가는 톱니가 0 인경우 return
+        if(gearNo == 0)
+            return ;
+
+        // 돌아가는 바퀴와 왼쪽 바퀴의 극 확인 후 다른 경우 회전
+        if(gear[gearNo][6] != gear[gearNo-1][2]) {
+            checkLeft(gear, gearNo-1, -direction);
+            rotation(gear, gearNo-1, -direction);
         }
     }
 
-    private static void checkRotation(int[] left, int[] right, int[][] gear,boolean[] rotation) {
-        for(int i=0;i<3;i++) {
-            if(gear[left[i]] == gear[right[i]]) {
-                rotation[i] = false;
-                rotation[i+1] = false;
+    private static void rotation(int[][] gear, int gearNo, int direction) {
+        // 1인 경우 시계방향 회전
+        if(direction == 1) {
+            int temp = gear[gearNo][7];
+            for(int i=6;i>=0;i--) {
+                gear[gearNo][i+1] = gear[gearNo][i];
             }
-            else {
-                rotation[i] = true;
-                rotation[i+1] = true;
+            gear[gearNo][0] = temp;
+        }
+        // -1인 경우 반시계 방향 회전
+        else {
+            int temp = gear[gearNo][0];
+            for(int i=0;i<7;i++) {
+                gear[gearNo][i] = gear[gearNo][i+1];
             }
+            gear[gearNo][7] = temp;
         }
     }
-    /*private static void checkRotation(int[] left, int[] right, int gearNo, boolean[] rotation) {
-        if (gearNo == 1) {
-            // 1번 오른쪽, 2번 왼쪽 /  2번 오른쪽 3번 왼쪽 / 3번 오른쪽 4번 왼쪽
 
-        } else if (gearNo == 2) {
-            // 2번 왼쪽 , 1번 오른족 / 2번 오른쪽 3번 왼쪽 / 3번 오른쪽 4번 왼쪽
-        } else if (gearNo == 3) {
-            // 3번 오른쪽, 4번 왼쪽, 3번 왼쪽, 2번 오른쪽, 2번 왼쪽 , 1번 오른쪽
-        } else {
-            // 4번 왼쪽, 3번 오른쪽, 3번 왼쪽 2번 오른쪽, 2번 오니쪽, 1번 오른쪽
-        }
-    }*/
 }
