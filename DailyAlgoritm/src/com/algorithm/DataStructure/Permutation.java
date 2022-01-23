72983887f969c139d97c2925c861408c5ca9b9b8
zayson <jymaeng95@gmail.com>
2022-01-23 16:39:17 +0900

    /* 22.01.23 Daily Algorithm */

--- DailyAlgoritm/src/com/algorithm/DataStructure/Permutation.java
+++ DailyAlgoritm/src/com/algorithm/DataStructure/Permutation.java
@@ -21,7 +21,7 @@ public class Permutation {
         }
 
         for (int i = 0; i < n; i++) {
-            if (visited[i] != true) {
+            if (!visited[i]) {
                 visited[i] = true;
                 output[depth] = arr[i];
                 perm(arr, output, visited, depth + 1, n, r);
