586d65252161b7ecd4b6979dae9eeb0a88f2dd81
zayson <jymaeng95@gmail.com>
2022-02-01 22:59:25 +0900

    /* 22.02.01 Daily Algorithm */

--- DailyAlgoritm/src/com/algorithm/Baekjoon/Main_1516_게임_개발.java
+++ DailyAlgoritm/src/com/algorithm/Baekjoon/Main_1516_게임_개발.java
@@ -3,74 +3,58 @@ package com.algorithm.Baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main_1516_게임_개발 {
-    static class Node {
-        private int start;
-        private int end;
-        private int time;
 
-        public Node(int start, int end, int time) {
-            this.start = start;
-            this.end = end;
-            this.time = time;
-        }
-
-        public int getStart() {
-            return start;
-        }
-
-        public int getEnd() {
-            return end;
-        }
-
-        public int getTime() {
-            return time;
-        }
-    }
-    private static List<Node> edge;
+    private static List<List<Integer>> graph;
     public static void main(String[] args) throws IOException {
         BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        int[] parent = new int[N+1];
-        int[] time = new int[N+1];
-        for(int i=0;i<N+1;i++) {
-            parent[i] = i;
+        int[] indegree = new int[N+1];
+        int[] second = new int[N+1];
+        graph = new ArrayList<>();
+        for(int i=0;i<N;i++) {
+            graph.add(new LinkedList<>());
         }
-
         for(int i=0;i<N;i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int second = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
-            if(end == -1) end = i+1;
-            time[i+1] = second;
-            edge.add(new Node(i+1, end, second));
+            int time = Integer.parseInt(st.nextToken());
+            second[i+1] = time;
+            while(st.hasMoreTokens()) {
+                int end = Integer.parseInt(st.nextToken());
+                if(end == -1) break;
+                graph.get(i).add(end);
+                indegree[i+1]++;
+            }
         }
 
-        int[] rst = new int[N+1];
-        developGame(N, time, parent, rst);
+        int[] rst = new int[N];
+        developGame(N, second, indegree, rst);
+        for(int x : rst) {
+            System.out.println("x = " + x);
+        }
         br.close();
     }
 
-    private static void developGame(int n, int[] time, int[] parent, int[] rst) {
-        for(Node node : edge) {
-            int start = node.getStart();
-            int end = node.getEnd();
-            int second = node.getTime();
-
-//            if(find(start, parent, time) != find(end,parent, time)) {
-//                union()
-//            }
+    private static void developGame(int n, int[] second, int[] indegree, int[] rst) {
+        Queue<Integer> queue = new LinkedList<>();
+        for(int i=0;i<indegree.length;i++ ) {
+            if(indegree[i] == 0) {
+                queue.add(i);
+            }
         }
 
-    }
-
-    private static int find(int x, int second, int[] parent, int[] time) {
-        if(parent[x] != x) {
-            parent[x] = find(x, second, parent, time);
+        int i=0;
+        while(!queue.isEmpty()) {
+            int vertex = queue.poll();
+            for(int v : graph.get(vertex)) {
+                indegree[v]--;
+                if(indegree[v] == 0) {
+                    queue.add(v);
+                    rst[v] = second[v]+second[vertex];
+                }
+            }
         }
-        return parent[x];
     }
 }
