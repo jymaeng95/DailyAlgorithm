3ae76b6f7c3c7c94c92a04ef423c011b2aa7ec68
zayson <jymaeng95@gmail.com>
2022-04-06 22:56:43 +0900

    /* 22.04.06 Daily Algorithm */œ

--- DailyAlgoritm/src/com/algorithm/Baekjoon/Main_2156_포도주_시식.java
+++ DailyAlgoritm/src/com/algorithm/Baekjoon/Main_2156_포도주_시식.java
@@ -5,34 +5,30 @@ import java.io.*;
 public class Main_2156_포도주_시식 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
         int N = Integer.parseInt(br.readLine());
-        int[] wine = new int[N];
-        for (int i = 0; i < N; i++) {
-            wine[i] = Integer.parseInt(br.readLine());
+        int[] wine = new int[N + 1];
+        for (int index = 1; index <= N; index++) {
+            wine[index] = Integer.parseInt(br.readLine());
+        }
+        int maxWine = wine[1];
+        if (N > 1) {
+            maxWine = getMaxWine(N, wine);
         }
 
-        int rst = getMaxWine(N, wine);
-        bw.write(String.valueOf(rst));
-        bw.flush();
+        System.out.println(maxWine);
+
         br.close();
-        bw.close();
     }
 
     private static int getMaxWine(int n, int[] wine) {
-        int[] dp = new int[n+1];
-        dp[0] = 0;
-        dp[1] = wine[0];
-        dp[2] = wine[0] + wine[1];
+        int[] dp = new int[n + 1];
+        dp[1] = wine[1];
+        dp[2] = wine[1] + wine[2];
 
-        for(int i = 3; i < dp.length; i++) {
-            dp[i] = Math.max(dp[i-2] + wine[i-1], Math.max(wine[i-1] + wine[i-2], dp[i-3] + wine[i-2] + wine[i-1]));
-        }
-
-        int max = 0;
-        for(int x : dp) {
-            max = Math.max(max, x);
+        int max = Math.max(dp[1], dp[2]);
+        for (int index = 3; index <= n; index++) {
+            dp[index] = Math.max(dp[index-1],wine[index] + Math.max(wine[index - 1] + dp[index - 3], dp[index - 2]));
+            max = Math.max(dp[index], max);
         }
 
         return max;
