6d172bf6f96396370fed9c3c7940284b3bfb0fe7
zayson <jymaeng95@gmail.com>
2022-04-19 22:07:37 +0900

    /* 22.04.19 Daily Algorithm */

--- DailyAlgoritm/src/com/algorithm/Baekjoon/Main_16637_괄호_추가하기.java
+++ DailyAlgoritm/src/com/algorithm/Baekjoon/Main_16637_괄호_추가하기.java
@@ -17,14 +17,14 @@ public class Main_16637_괄호_추가하기 {
             else op[index / 2] = exp[index];
         }
 
-        int rst = getMaxResult(N, num, op);
+        int rst = getMaxResult(num, op);
         System.out.println(rst);
 
         br.close();
     }
 
 
-    private static int getMaxResult(int n, int[] num, char[] op) {
+    private static int getMaxResult(int[] num, char[] op) {
         result = Integer.MIN_VALUE;
 
         makeExp(num[0], 0, num, op);
@@ -36,16 +36,21 @@ public class Main_16637_괄호_추가하기 {
     private static void makeExp(int output, int index, int[] num, char[] op) {
         // 연산자 모두 사용한 경우
         if (index >= op.length) {
+            // 최대값 갱신
             result = Math.max(result, output);
             return;
         }
 
         // 괄호 계산 하는 경우
+        // 숫자 배열이 넘어가지 않도록
         if (index + 2 < num.length) {
+            // 현재 숫자 배열 인덱스보다 다음 두개의 숫자의 괄호 계산
             int rst = calc(num[index + 1], op[index + 1], num[index + 2]);
+            // 현재까지의 ouput 과 다음 두개 계산 값 계산
             makeExp(calc(output, op[index], rst), index + 2, num, op);
         }
         // 괄호 계산 하지 않는 경우
+        // 현재까지의 output과 다음 숫자 계산
         int rst = calc(output, op[index], num[index+1]);
         makeExp(rst, index + 1, num ,op);
     }
