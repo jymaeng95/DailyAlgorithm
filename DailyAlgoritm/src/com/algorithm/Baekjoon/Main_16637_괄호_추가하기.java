70c09ff0ede5068192d5472436c9143374707f30
zayson <jymaeng95@gmail.com>
2022-04-19 13:39:16 +0900

    /* 22.04.19 Daily Algorithm */

--- DailyAlgoritm/src/com/algorithm/Baekjoon/Main_16637_괄호_추가하기.java
+++ DailyAlgoritm/src/com/algorithm/Baekjoon/Main_16637_괄호_추가하기.java
@@ -3,7 +3,6 @@ package com.algorithm.Baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Stack;
 
 public class Main_16637_괄호_추가하기 {
     public static void main(String[] args) throws IOException {
@@ -24,65 +23,34 @@ public class Main_16637_괄호_추가하기 {
         br.close();
     }
 
-    private static Stack<String> exp;
 
     private static int getMaxResult(int n, int[] num, char[] op) {
         result = Integer.MIN_VALUE;
-        exp = new Stack<>();
 
-        exp.add(String.valueOf(num[0]));
-        makeExp(0, num, op);
+        makeExp(0, 0, num, op);
         return result;
     }
 
     private static int result;
 
-    private static void makeExp(int index, int[] num, char[] op) {
-        if (index == op.length) {
-            if (exp.size() == 1) result = Math.max(result, Integer.parseInt(exp.peek()));
-            else calculate();
-
+    private static void makeExp(int output, int index, int[] num, char[] op) {
+        // 연산자 모두 사용한 경우
+        if (index >= op.length) {
+            result = Math.max(result, output);
             return;
         }
 
-        // stack top 과 index op, index+1 num 계산 후 stack에 넣기
-        if (op[index] == '+') {
-            int top = Integer.parseInt(exp.pop());
-            exp.push(String.valueOf(top + num[index + 1]));
-            makeExp(index + 1, num, op);
-            // 백트래킹으로 계산 반대로
-            exp.push(String.valueOf(Integer.parseInt(exp.pop()) - num[index + 1]));
-        } else if (op[index] == '-') {
-            int top = Integer.parseInt(exp.pop());
-            exp.push(String.valueOf(top - num[index + 1]));
-            makeExp(index + 1, num, op);
-            // 백트래킹으로 계산 반대로
-            exp.push(String.valueOf(Integer.parseInt(exp.pop()) + num[index + 1]));
-        } else {
-            int top = Integer.parseInt(exp.pop());
-            exp.push(String.valueOf(top * num[index + 1]));
-            makeExp(index + 1, num, op);
-            // 백트래킹으로 계산 반대로
-            if(num[index+1] != 0)
-                exp.push(String.valueOf(Integer.parseInt(exp.pop()) / num[index + 1]));
-        }
+        // 괄호 계산 하는 경우
+        int rst = calc(num[index], op[index], num[index + 1]);
+        makeExp(rst, index + 2, num, op);
 
-        // 수식 그대로 넣기
-        exp.push(String.valueOf(op[index]));
-        exp.push(String.valueOf(num[index + 1]));
-        makeExp(index + 1, num, op);
-        exp.pop();
-        exp.pop();
+        // 괄호 계산 하지 않는 경우
+
     }
 
-    private static void calculate() {
-        int rst = Integer.parseInt(exp.get(0));
-        for (int index = 1; index < exp.size(); index += 2) {
-            if (exp.get(index).equals("+")) rst += Integer.parseInt(exp.get(index + 1));
-            if (exp.get(index).equals("-")) rst -= Integer.parseInt(exp.get(index + 1));
-            if (exp.get(index).equals("*")) rst *= Integer.parseInt(exp.get(index + 1));
-        }
-
-        result = Math.max(result, rst);
+    private static int calc(int prev, char operation, int post) {
+        if (operation == '+') return prev + post;
+        if (operation == '-') return prev - post;
+        if (operation == '*') return prev * post;
     }
 }
