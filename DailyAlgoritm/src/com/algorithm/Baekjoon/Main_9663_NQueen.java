5bfc523becd65c99898e1d42e1a1b57950980f0d
zayson <jymaeng95@gmail.com>
2022-05-15 23:21:12 +0900

    /* 22.05.15 Daily Algorithm */

---
+++ DailyAlgoritm/src/com/algorithm/Baekjoon/Main_9663_NQueen.java
@@ -0,0 +1,7 @@
+package com.algorithm.Baekjoon;
+
+public class Main_9663_NQueen {
+    public static void main(String[] args) {
+
+    }
+}
--- DailyAlgoritm/src/com/algorithm/Baekjoon/Main_9663_N_Queen.java
+++ DailyAlgoritm/src/com/algorithm/Baekjoon/Main_9663_N_Queen.java
@@ -8,13 +8,14 @@ public class Main_9663_N_Queen {
     private static boolean[] visitedCrossUp;
     private static boolean[] visitedCrossDown;
     private static int count;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         N = Integer.parseInt(br.readLine());
         count = 0;
-        visitedCrossDown = new boolean[2*N-1];
-        visitedCrossUp = new boolean[2*N-1];
+        visitedCrossDown = new boolean[2 * N - 1];
+        visitedCrossUp = new boolean[2 * N - 1];
         visitedColumn = new boolean[N];
         NQueen(0);
 
@@ -24,20 +25,20 @@ public class Main_9663_N_Queen {
     }
 
     private static void NQueen(int cnt) {
-        if(cnt == N) {
+        if (cnt == N) {
             count++;
             return;
         }
-        for(int i=0;i<N;i++){
-            if(visitedColumn[i] || visitedCrossUp[i+cnt] || visitedCrossDown[cnt-i+N-1])
+        for (int i = 0; i < N; i++) {
+            if (visitedColumn[i] || visitedCrossUp[i + cnt] || visitedCrossDown[cnt - i + N - 1])
                 continue;
             visitedColumn[i] = true;
-            visitedCrossUp[i+cnt] = true;
-            visitedCrossDown[cnt-i+N-1] = true;
-            NQueen(cnt+1);
+            visitedCrossUp[i + cnt] = true;
+            visitedCrossDown[cnt - i + N - 1] = true;
+            NQueen(cnt + 1);
             visitedColumn[i] = false;
-            visitedCrossUp[i+cnt] = false;
-            visitedCrossDown[cnt-i+N-1] = false;
+            visitedCrossUp[i + cnt] = false;
+            visitedCrossDown[cnt - i + N - 1] = false;
         }
     }
 }
