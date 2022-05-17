c65aff517be4eb0a3fb0b03e2233d27cd0aee694
zayson <jymaeng95@gmail.com>
2022-05-17 18:30:19 +0900

    /* 22.05.17 Daily Algorithm */

--- DailyAlgoritm/src/com/algorithm/Baekjoon/Main_1052_물병.java
+++ DailyAlgoritm/src/com/algorithm/Baekjoon/Main_1052_물병.java
@@ -13,35 +13,9 @@ public class Main_1052_물병 {
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
 
-//        int rst = buyBottle(N, K);
         int rst = buyBottleCount(N, K);
         System.out.println(rst);
         br.close();
-    }
-
-    // 실패 코드
-    private static int buyBottle(int n, int k) {
-        /**
-         * 1. K-1번 돌면서 남은 물병 보다 작은 2의 base 제곱 값 빼주기 (최대한 많은 물병을 이용해 하나로 만들기)
-         * 2. 마지막은 해당 수보다 큰 2의 base 제곱 값 구해서 남은 물병 빼주기
-         */
-        if(n <= k) return 0;
-
-        // 1. K-1번 돌면서 남은 물병 보다 작은 2의 base 제곱 값 빼주기 (최대한 많은 물병을 이용해 하나로 만들기)
-        for (int loop = 0; loop < k - 1; loop++) {
-            int base = 0;
-            while (Math.pow(2, base) < n) {
-                base++;
-            }
-            n -= Math.pow(2, base - 1);
-        }
-
-        // 2. 마지막은 해당 수보다 큰 2의 base 제곱 값 구해서 남은 물병 빼주기
-        int base = 0;
-        while (Math.pow(2, base) < n) {
-            base++;
-        }
-        return (int) Math.pow(2, base) - n;
     }
 
     // 정답 코드
