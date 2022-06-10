ed9f55599e578af11e6ee5e7aaa7be5007f06f2b
zayson <jymaeng95@gmail.com>
2022-06-10 12:06:16 +0900

    /* 22.06.10 Daily Algorithm */

--- DailyAlgoritm/src/com/algorithm/Baekjoon/Main_6087_레이저_통신.java
+++ DailyAlgoritm/src/com/algorithm/Baekjoon/Main_6087_레이저_통신.java
@@ -4,12 +4,11 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main_6087_레이저_통신 {
-    private static int startRow, startCol;
+    private static int startRow, startCol, endRow, endCol;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -19,12 +18,19 @@ public class Main_6087_레이저_통신 {
         int N = Integer.parseInt(st.nextToken());
 
         String[][] map = new String[N][M];
+        boolean start = false;
         for (int row = 0; row < N; row++) {
             String[] split = br.readLine().split("");
             for (int col = 0; col < M; col++) {
                 if (split[col].equals("C")) {
-                    startRow = row;
-                    startCol = col;
+                    if (!start) {
+                        startRow = row;
+                        startCol = col;
+                        start = true;
+                    } else {
+                        endRow = row;
+                        endCol = col;
+                    }
                 }
                 map[row][col] = split[col];
             }
@@ -40,11 +46,10 @@ public class Main_6087_레이저_통신 {
     private static final int[] DY = {0, 0, 1, -1};
 
     private static int settingMirror(int n, int m, String[][] map) {
-//        PriorityQueue<Road> pq = new PriorityQueue<>();
-        Queue<Road> pq = new LinkedList<>();
+        PriorityQueue<Road> pq = new PriorityQueue<>();
         int[][] mirror = new int[n][m];
         for (int row = 0; row < n; row++) {
-            Arrays.fill(mirror[row], Integer.MAX_VALUE);
+            Arrays.fill(mirror[row], n*m);
         }
 
         pq.add(new Road(startRow, startCol, -1, 0));
@@ -55,9 +60,9 @@ public class Main_6087_레이저_통신 {
             Road curRoad = pq.poll();
 
             // C에 도착하고 ,시작 행 열이 아닌 경우
-            if (map[curRoad.getRow()][curRoad.getCol()].equals("C") && curRoad.getRow() != startRow && curRoad.getCol() != startCol) {
+            if (curRoad.getRow() == endRow && curRoad.getCol() == endCol) {
                 mirrorCount = Math.min(mirrorCount, mirror[curRoad.getRow()][curRoad.getCol()]);
-                continue;
+                break;
             }
 
             for (int direction = 0; direction < 4; direction++) {
@@ -66,14 +71,17 @@ public class Main_6087_레이저_통신 {
 
                 if (checkBound(nextRow, nextCol, n, m) && !map[nextRow][nextCol].equals("*")) {
                     // 최초 방향이 아니고, 이전 방향과 현재 방향이 다르면
-                    if (curRoad.getDirection() != -1 && direction != curRoad.getDirection() && mirror[nextRow][nextCol] >= mirror[curRoad.getRow()][curRoad.getCol()] + 1) {
-                        // 거울 개수 한 개 추가
-                        mirror[nextRow][nextCol] = mirror[curRoad.getRow()][curRoad.getCol()] + 1;
-                        pq.offer(new Road(nextRow, nextCol, direction, mirror[nextRow][nextCol]));
-                    }
-                    else if ((curRoad.getDirection() == -1 || direction == curRoad.getDirection()) && mirror[nextRow][nextCol] >= mirror[curRoad.getRow()][curRoad.getCol()]) {
-                        mirror[nextRow][nextCol] = mirror[curRoad.getRow()][curRoad.getCol()];
-                        pq.offer(new Road(nextRow, nextCol, direction, mirror[nextRow][nextCol]));
+                    if (curRoad.getDirection() != -1 && direction != curRoad.getDirection()) {
+                        if(mirror[nextRow][nextCol] >= curRoad.getMirror()+1) {
+                            // 거울 개수 한 개 추가
+                            mirror[nextRow][nextCol] = curRoad.getMirror() + 1;
+                            pq.offer(new Road(nextRow, nextCol, direction, mirror[nextRow][nextCol]));
+                        }
+                    } else {
+                        if (mirror[nextRow][nextCol] >= curRoad.getMirror()) {
+                            mirror[nextRow][nextCol] = curRoad.getMirror();
+                            pq.offer(new Road(nextRow, nextCol, direction, mirror[nextRow][nextCol]));
+                        }
                     }
                 }
             }
