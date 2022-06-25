dbc4f707659905f1ec03bccb7b897ffce6e8aa7f
zayson <jymaeng95@gmail.com>
2022-06-26 00:03:42 +0900

    /* 22.06.26 Daily Algorithm */

--- DailyAlgoritm/src/com/algorithm/Baekjoon/Main_2257_화학식량.java
+++ DailyAlgoritm/src/com/algorithm/Baekjoon/Main_2257_화학식량.java
@@ -21,19 +21,19 @@ public class Main_2257_화학식량 {
 
         for (char chemical : exp.toCharArray()) {
             // 화학식 변경
-            if(chemical == 'H') stack.push("1");
-            else if(chemical == 'C') stack.push("12");
-            else if(chemical == 'O') stack.push("16");
-            else if(chemical == '(') stack.push("(");
+            if (chemical == 'H') stack.push("1");
+            else if (chemical == 'C') stack.push("12");
+            else if (chemical == 'O') stack.push("16");
+            else if (chemical == '(') stack.push("(");
             // 숫자인 경우 가장 위의 스택에 숫자 곱한 후 넣어주기
-            else if(!stack.isEmpty() && Character.isDigit(chemical)) {
+            else if (!stack.isEmpty() && Character.isDigit(chemical)) {
                 int combination = Integer.parseInt(stack.pop()) * (chemical - '0');
                 stack.push(String.valueOf(combination));
             }
             // 괄호가 닫히는 경우 괄호 열린 곳까지 더해서 열린 괄호 지우고 더한 값 넣어주기
-            else if(chemical == ')') {
+            else if (chemical == ')') {
                 int combination = 0;
-                while(!stack.isEmpty() && !stack.peek().equals("(")) {
+                while (!stack.isEmpty() && !stack.peek().equals("(")) {
                     combination += Integer.parseInt(stack.pop());
                 }
                 stack.pop(); // (없애기
@@ -41,7 +41,7 @@ public class Main_2257_화학식량 {
             }
         }
 
-        while(!stack.isEmpty()) {
+        while (!stack.isEmpty()) {
             sum += Integer.parseInt(stack.pop());
         }
 
