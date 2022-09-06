64171c543feb20f6c1476cf3fc16c89f0624b69b
zayson <jymaeng95@gmail.com>
2022-09-06 21:08:32 +0900

    feat: [그리디, 큐] 두 큐 합계 같게 만들기

--- DailyAlgoritm/src/com/second/algorithm/programmers/Solution_118667_두_큐_합계_같게_만들기.java
+++ DailyAlgoritm/src/com/second/algorithm/programmers/Solution_118667_두_큐_합계_같게_만들기.java
@@ -19,34 +19,43 @@ public class Solution_118667_두_큐_합계_같게_만들기 {
         long sum1 = Arrays.stream(queue1).sum();
         long sum2 = Arrays.stream(queue2).sum();
 
-        // 두 큐의 합계가 홀수인 경우 만들 수 없음
+        // 두 큐의 합이 홀수인 경우 동일하게 만드는 것 불가능
         if((sum1 + sum2) % 2 != 0) return -1;
 
-        // 합계 동일한 경우
+        // 두 큐의 합이 동일한 경우
         if(sum1 == sum2) return 0;
 
-        long target = (sum1 + sum2) / 2;
-        Queue<Integer> q1 = new LinkedList<>();
-        Arrays.stream(queue1).forEach(q1::add);
 
-        Queue<Integer> q2 = new LinkedList<>();
-        Arrays.stream(queue2).forEach(q2::add);
-        int move = 0;
-        while(!q1.isEmpty() && !q2.isEmpty()) {
-            if(sum1 < target) {
-                // 큐1의 합이 타겟 보다 작은 경우 큐2에서 빼서 넣어주기
-                int poll = q2.poll();
-                q1.add(poll);
-                sum1 += poll;
-                move++;
-            } else if(sum1 > target) {
-                // 큐1에서 빼서 큐2에 넣어주기
-                int poll = q1.poll();
-                q2.add(poll);
-                sum1 -= poll;
-                move++;
+        Queue<Integer> queueA = new LinkedList<>();
+        Queue<Integer> queueB = new LinkedList<>();
+
+        Arrays.stream(queue1).forEach(queueA::add);
+        Arrays.stream(queue2).forEach(queueB::add);
+
+        int count = 0, idxA = 0;
+        int length = queue1.length + queue2.length;
+
+        // 두 큐중 하나가 비는 경우 만들 수 없는 경우 두 큐중 하나가 큐 두개를 거쳐서 자신의 큐로 돌아온다면 만들 수 없는 경우
+        while(!queueA.isEmpty() && !queueB.isEmpty() && idxA < length) {
+            // 합계가 작은 큐에 큰 큐에서 poll해서 넣어줌
+            if(sum1 > sum2) {
+                // A -> B
+                int number = queueA.poll();
+                sum1 -= number;
+                sum2 += number;
+
+                queueB.add(number);
+                idxA++;
+            } else if(sum1 < sum2) {
+                // B->A
+                int number = queueB.poll();
+                sum1 += number;
+                sum2 -= number;
+
+                queueA.add(number);
             }
-            else return move;
+            else return count;
+            count ++;
         }
 
         return -1;
