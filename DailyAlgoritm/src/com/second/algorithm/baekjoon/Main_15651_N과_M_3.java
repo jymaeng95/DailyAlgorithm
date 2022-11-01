592409bd45003a69860aabddb77a4d09c2b9f913
zayson <jymaeng95@gmail.com>
2022-11-01 17:02:21 +0900

    feat: [완전탐색] N과 M(3)

---
+++ DailyAlgoritm/src/com/second/algorithm/baekjoon/Main_15651_N과_M_3.java
@@ -0,0 +1,42 @@
+package com.second.algorithm.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_15651_N과_M_3 {
+    private static int N, M;
+    private static final StringBuilder builder = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        int[] nums = new int[M];
+
+        makeSubsequence(0, nums);
+        System.out.println(builder.toString());
+
+        br.close();
+    }
+
+    private static void makeSubsequence(int count, int[] nums) {
+        if (count == M) {
+            for (int num : nums) {
+                builder.append(num).append(" ");
+            }
+            builder.append("\n");
+
+            return;
+        }
+
+        for (int num = 1; num <= N; num++) {
+            nums[count] = num;
+            makeSubsequence(count + 1, nums);
+        }
+    }
+}
